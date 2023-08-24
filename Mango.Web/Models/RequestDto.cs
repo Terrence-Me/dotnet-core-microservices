82f86a1b7ec52724afc9a526a9220af3bf930f25
Terrence Meikle <terrence_meikle@yahoo.com>
2023-08-24 05:12:37 -0400

    Add project files.

---
+++ Mango.Web/Models/RequestDto.cs
@@ -0,0 +1,14 @@
+﻿using static Mango.Web.Utility.SD;
+
+namespace Mango.Web.Models
+{
+    public class RequestDto
+    {
+        public ApitType ApiType { get; set; } = ApitType.GET;
+        public string Url { get; set; }
+
+        public object Data { get; set; }
+
+        public string AccessToken { get; set; }
+    }
+}
---
+++ Mango.Web/Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Web.Utility
+{
+    public class SD
+    {
+        public enum ApitType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
